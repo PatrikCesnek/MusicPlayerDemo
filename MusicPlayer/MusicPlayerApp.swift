b4a7a699ec206e309a9085b4bf7e7d4ba0e04dea
Patrik Cesnek <pcesnek290@gmail.com>
2025-04-12 15:43:03 +0200

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Patrik Cesnek on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
