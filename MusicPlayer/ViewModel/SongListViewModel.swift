bade3fab69668652e1b3f0403e735d14bf732461
Patrik Cesnek <pcesnek290@gmail.com>
2025-04-12 17:34:54 +0200

    Create SongListViewModel

---
+++ MusicPlayer/ViewModel/SongListViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  SongListViewModel.swift
+//  MusicPlayer
+//
+//  Created by Patrik Cesnek on 12/04/2025.
+//
+
+import Foundation
+import Observation
+
+@Observable
+class SongListViewModel {
+    var songs: [Song] = []
+    var isLoading = false
+    var error: String? = nil
+    
+    func loadSongs() {
+        isLoading = true
+        error = nil
+        
+        APIService.shared.fetchSongs { result in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                switch result {
+                case .success(let songs):
+                    self.songs = songs
+                case .failure(let error):
+                    self.error = error.localizedDescription
+                }
+            }
+        }
+    }
+}
